17a313cca038935dde09c76431f7c248da19111a
raissalotin <rahlotin@gmail.com>
2023-04-14 16:59:17 -0300

    so

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,70 @@
 <script setup>
 import { ref } from 'vue'
 
-const aceitaTermos = ref(false)
+const nome = ref('')
+const data = ref('')
+const email = ref ('')
+const senha = ref ('')
+const confirma = ref ('')
+const estado = ref('')
+const endereco = ref('')
+const cidade = ref('')
+const hobby = ref('')
+const linguagem = ref ('')
+const biografia = ref('')
+const mostrar = ref (false)
+
+
 
 </script>
 
 <template>
   <form @submit.prevent="">
-  <label for="aceitaTermos">Aceito os termos</label>
-  <input type="checkbox" v-model="aceitaTermos" />
-  <p>Aceita os termos: </p>
-  <label for="sexo">Sexo</label>
-  <input type="radio" v-model="sexo" value="masculino" /> Masculino
-  <input type="radio" v-model="sexo" value="feminino" /> Feminino
-  <p>Sexo: </p>
+    
+   
+    <label for="text">Nome: </label>
+    <input type="text" v-on:keypress="ok= false" v-model="nome" required placeholder="digite seu nome" /> <br>
+    <p>o nome digitado é: {{ nome }}</p>
+    <br>
+    <label for="date">Data de nascimento: </label>
+    <input type="date" v-on:keypress="ok= false" v-model="data" required placeholder="data de nascimento" /> <br>
+    <p>A data é : {{ data }}</p>
+<br>
+   <label for="text">endereço: </label>
+    <input type="text" v-on:keypress="ok=false" v-model="endereco" required placeholder="Digite seu endereço" />
+    <p>O endereço digitado é: {{ endereco }}</p>
+  
+<br>
+    <label for="text">cidade: </label>
+    <input type="text" v-on:keypress="ok=false" v-model="cidade" required placeholder="Digite sua cidade" />
+    <p>A cidade digitada é: {{ cidade }}</p>
+<br>
+
+    <label for="email">Email: </label>
+    <input type="email" v-on:keypress="ok= false" v-model="email" required placeholder="digite seu email"/> <br>
+    <p>o email digitado é: {{ email }}</p>
+<br>
+<label for="text">Hobby: </label>
+    <input type="text" v-on:keypress="ok= false" v-model="hobby" required placeholder="digite seu hobby"/> <br>
+    <p>o hobby digitado é: {{ hobby}}</p>
+    <br>
+    
+    <label for="text">Linguagem de programação: </label>
+    <input type="text" v-on:keypress="ok=false" v-model="linguagem" required placeholder="Digite sua linguagem" />
+    <p>A linguagem digitada é: {{ linguagem }}</p>
+    <br>
+
+    <label for="text">biografia: </label>
+    <input type="text" v-on:keypress="ok=false" v-model="biografia" placeholder="Digite sua biografia" />
+    <p>A biografia digitada é: {{ biografia }}</p>
+    <br>
+
+
+
+
+   
   <label for="Estados">Estado</label>
-  <select v-model="Estados">
+  <select v-on:keypress="ok =false" v-model="estado">
     
     <option value="12">AC - Acre</option>
     <option value="27">AL - Alagoas</option>
@@ -44,14 +93,45 @@ const aceitaTermos = ref(false)
     <option value="35">SP - São Paulo</option>
     <option value="28">SE - Sergipe</option>
     <option value="17">TO - Tocantins</option>
+  </select> 
+  <p>O estado escolhido é é: {{ estado }}</p>
+  <br>
+
+  <label for="password">senha: </label>
+    <input type="password" v-on:keypress="ok= false" v-model="senha" required placeholder="digite sua senha"/><br>
+    <p>a senha digitada é: {{ senha }} </p>
+    <br>
+    <label for="password">confirmação de senha: </label>
+    <input type="password" v-on:keypress="ok= false" v-model="confirma" required placeholder="digite novamente"/><br>
+    <p>a confirmação digitada é: {{ confirma}}</p>
+    <br>
     
-
-
-
-    
-   
-  </select>
-  <p>Estado: </p>
-  <button type="submit"></button>
+    <button @click="mostrar = !mostrar"> confirmar</button>
+  
 </form>
+
+<div v-if="mostrar">
+<p>o nome digitado é: {{ nome }}</p>
+<br>
+<p>o endereço é: {{ endereco}}</p>
+<br>
+<p>a cidade digitada é: {{ cidade}}</p>
+<br>
+<p>o email digitado é: {{ email}}</p>
+<br>
+<p>o hobby digitado é: {{ hobby }}</p>
+<br>
+<p>a linguagem de programação é: {{ linguagem}}</p>
+<br>
+<p>a biografia digitada é: {{ biografia}}</p>
+<br>
+<p>o estado escolhido é: {{estado}}</p>
+<br>
+<p>a senha digitada é: {{ senha }}</p>
+<br>
+<p>a confirmação digitada é:{{ confirma}}</p>
+<br>
+
+</div>
+
 </template>
